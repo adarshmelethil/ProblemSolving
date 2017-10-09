2f65ae34586c0953b7aff2593cc1e8f74d71e503
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-09 15:54:02 -0600

    finished problem 7

---
+++ ProjectEuler/7_10001stPrime/PrimeList.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#MultiplesOf3And5.py
+
+import sys
+import argparse
+
+FLAGS = None
+
+def getPrimes(number_of_primes):
+	primes_list = [2]
+	if number_of_primes == 1:
+		return primes_list
+	num = 3
+	while(len(primes_list) < number_of_primes):
+		is_prime = True
+		for prime in primes_list:
+			if num % prime == 0:
+				is_prime = False
+				break
+		if is_prime:
+			primes_list.append(num)
+		num += 2
+
+	return primes_list
+def main(_):
+	print(getPrimes(FLAGS.number_of_primes))
+			
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(description='Mapper to calculate n-gram')
+	parser.add_argument('-n','--number_of_primes', action='store', type=int, default=10001)
+
+
+	FLAGS, unparsed = parser.parse_known_args()
+	main(unparsed)
