bf740d099b3e5c5bd19c2fcd905760f0926ff371
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-09 15:32:19 -0600

    finished problem 6

---
+++ ProjectEuler/6_SumSquareDifference/SumSquareDifference.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#MultiplesOf3And5.py
+
+import sys
+import argparse
+
+FLAGS = None
+
+def main(_):
+	total = 0
+	for i in range(1, FLAGS.numbers+1):
+		for j in range(1, FLAGS.numbers+1):
+			if i == j:
+				continue
+			product = i * j
+			total += product
+	print(total)
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(description='Mapper to calculate n-gram')
+	parser.add_argument('-n','--numbers', action='store', type=int, default=100)
+
+
+	FLAGS, unparsed = parser.parse_known_args()
+	main(unparsed)
