43cb2bd3b3b782936a1958990b357858571cca8f
madcowken <adarshmelethil@gmail.com>
2017-12-27 05:39:56 -0700

    started red knights shortest path

--- HackerRank/World_CodeSprint_12/Red_Knights_Shortest_Path/RedKnightsShortestPath.py
+++ HackerRank/World_CodeSprint_12/Red_Knights_Shortest_Path/RedKnightsShortestPath.py
@@ -16,28 +16,38 @@ def printShortestPath(n, i_start, j_start, i_end, j_end):
     #  Print the distance along with the sequence of moves.
     upward_distance = (i_start - i_end)
     if  (upward_distance % 2) != 0 or \
-        ((j_start + ((upward_distance)//2))%2) == (j_end % 2): 
+        ((j_start + ((upward_distance)//2))%2) != (j_end % 2): 
         print("Impossible")
         return
     
     total_moves = 0
     moves = ""
     while i_start != i_end or j_start != j_end:
-
+        total_moves += 1
         if i_start > i_end:
-            total_moves += 1
-            i_start += 2
-            if j_start > j_end:
+            i_start -= 2
+            if j_start >= j_end:
                 moves += "UL "
                 j_start -= 1 
             else:
                 moves += "UR "
                 j_start += 1 
-        elif j_end > j_start:
+        elif i_start < i_end:
+            i_start += 2
+            if j_start >= j_end:
+                moves += "LL "
+                j_start -= 1 
+            else:
+                moves += "LR "
+                j_start += 1 
+        elif j_start < j_end:
             j_start += 2
-            moves += "R"
-
-        
+            moves += "R "
+        elif j_start > j_end:
+            j_start -= 2
+            moves += "L "
+    print(total_moves)
+    print(moves)
 
 
 
